17a8331d999bc83a354eb113d5a9eff195adcf42
Keshav Singh Yadav <keshavsinghyadav@Keshavs-MacBook-Pro.local>
2020-08-20 15:59:32 +0530

    init

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App title="React" />
   </React.StrictMode>,
   document.getElementById('root')
 );
